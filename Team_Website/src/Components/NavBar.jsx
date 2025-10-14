1e30aac450aa0fc0f8ecad20587e3912654c62f4
chima0bi <135566924+chima0bi@users.noreply.github.com>
2025-10-14 13:45:14 +0100

    changed head icon, 3rd commit

--- Team_Website/src/Components/NavBar.jsx
+++ Team_Website/src/Components/NavBar.jsx
@@ -1,6 +1,7 @@
 // import React from 'react'
 import "./NavBar.css";
-import logo from "../assets/img/wayne-logo.png";
+import logo from "../assets/img/wayne-logo2.png";
+import {Link} from "react-router-dom"
 
 const NavBar = () => {
   return (
@@ -9,10 +10,10 @@ const NavBar = () => {
         <img src={logo} className="logo" />
         <nav>
           <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/services">Services</a></li>
-            <li><a href="/contact">Contact Us</a></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/services">Services</Link></li>
+            <li><Link to="/contact">Contact Us</Link></li>
           </ul>
         </nav>
         <form action="serch.php" id="search-bar">
